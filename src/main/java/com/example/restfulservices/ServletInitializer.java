72fd14019bbcbf3de225b725b2cfe0307869cb2f
서원 <dev109@kakao.com>
2020-06-24 19:16:56 +0900

    HTTP Status Code 제어(ServletUriComponentsBuilder)

---
+++ src/main/java/com/example/restfulservices/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.restfulservices;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestfulServicesApplication.class);
+    }
+
+}
